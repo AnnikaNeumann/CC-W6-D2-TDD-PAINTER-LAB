8ed3eae993010a64a38ae7e68b23acc073d69e0a
Jack <jackowendickson@gmail.com>
2022-03-29 15:45:54 +0100

    completed paintCan tests

---
+++ paintCan.js
@@ -0,0 +1,17 @@
+const PaintCan = function (amount) {
+    this.amount = amount;
+}
+
+PaintCan.prototype.checkIfEmpty = function() {
+    if (this.amount == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+PaintCan.prototype.empty = function() {
+    this.amount = 0
+}
+
+module.exports = PaintCan;
--- specs/paintCan_spec.js
+++ specs/paintCan_spec.js
@@ -1,3 +1,24 @@
 const assert = require ('assert');
 const PaintCan = require ('../paintCan.js')
 
+describe('paintCan', function () {
+    let paintCan;
+
+    beforeEach(function(){
+        paintCan = new PaintCan(10);
+    });
+
+    it('should have amount of paint', function () {
+        const actual = paintCan.amount
+        assert.strictEqual(actual, 10)
+    })
+    it('should be able to check if it is empty', function () {
+        const actual = paintCan.checkIfEmpty();
+        assert.strictEqual(actual, false)
+    })
+    it('should be able to empty itself', function () {
+        paintCan.empty();
+        const actual = paintCan.checkIfEmpty();
+        assert.strictEqual(actual, true)
+    })
+})
